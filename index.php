c65d8bbb9ce565d0b1efa2910a9313114cf40922
Adetunji Adeyinka <55273527+Itzadetunji@users.noreply.github.com>
2021-08-20 04:17:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="col-md-12">
+			<img src="zuri.png" class="col-md-4" style="border-radius: 40px;">
+			<h3>Check out the internship company: <a href="https://zuri.team" target="_blank">Zuri</a></h3>
+			<h1 class="h1-responsive font-weight-bold">Resume</h1>
+			<h2 class="h2-responsive">Overview</h2>
+			<p>My name is Adetunji Adeyinka.<br> I am a Nigerian.<br> I am a 16-years-old developer with 3 years of experience.<br> I can:<br> - use HTML, CSS and Javascrpt for the frontend.<br> - use Frameworks (Bootstrap, W3CSS, JQuery) for fast development.<br> - use PHP and Python for the backend.<br> - use Flutter (a little) for building mobile apps.<br> - use MySQL for database management.<br> - use Heroku for DevOps, deployment and cloud computing. <br> - use Git/GitHub for efficient code management, code contribution and to set up continuous integration and development.<br> - use postman for testing APIs.<br> - build and use REST APIs.<br> <br> I also possess:<br> - fast learning abilities.<br> - team playing qualities ( currently the Labour Prefect of my high school (Caleb International College)).<br> - cyber security skills. <br> - keynote speaking qualities.<br> I was also the Head Boy for my primary school (The Pathway Schhol). <br> I have been the leader/ assistant for various projects.<br> I want to be the Elon Musk of Nigeria.
+			</p><br>
+			<h2 class="h1-responsive font-weight-bold">Educational History</h2>
+			<h3 class="h3-responsive">2006-2015 | The Pathway School</h3>
+			<p>I completed 9 years in elementary school and graduated as the best graduating student. I was in clubs such as the chess club and book club. I was also the head boy of my sceondary school.</p>
+			<hr>
+			<h3 class="h3-responsive">2015-2021 | Caleb International College</h3>
+			<p>I am currenly in my final year in high school. I involve myself in clubs such as the media crew, the chess club and the JETS club. I am also the Labour prefect of my school.</p>
+			<hr>
+			<h2 class="h1-responsive font-weight-bold">Expreience</h2>
+			<h3 class="h3-responsive">Feb 21 - July 21 | Zuri Internship Training</h3>
+			<p>I enrolled in an internship trainnig programme so as to make myself familiar with the work environment and prepare myself for upcoming internships and become more responsible in my work field.</p>
+		</div>
+		<section class="mb-4">
+		<h2 class="h2-responsive font-weight-bold text-center my-4">My socials</h2>
+	    <h2 class="h2-responsive text-center my-4 mb-3"><a href="https://twitter.com/itzadetunji1"target="_blank">Twitter</a> | <a href="https://instagram.com/itzadetunji">Instagram</a> | <a href="https://github.com/itzadetunji"target="_blank">Github</a></h2>
+	    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
+	    <p class="text-center w-responsive mx-auto mb-5">Send Me An E-mail.
+		    <div class="row">
+		        <div class="col-md-12 mb-md-0 mb-5">
+		            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+		                <div class="row">
+		                    <div class="col-md-6">
+		                        <div class="md-form mb-0">
+		                            <input type="text" id="name" name="name" class="form-control" placeholder="Your Full-Name">
+		                            <label for="name"></label>
+		                        </div>
+		                    </div>
+		                    <div class="col-md-6">
+		                        <div class="md-form mb-0">
+		                            <input type="text" id="email" name="email" class="form-control" placeholder="Your Email Address">
+		                            <label for="email"></label>
+		                        </div>
+		                    </div>
+		                </div>
+		                <div class="row">
+		                    <div class="col-md-12">
+		                        <div class="md-form mb-0">
+		                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject">
+		                            <label for="subject"></label>
+		                        </div>
+		                    </div>
+		                </div>
+		                <div class="row">
+		                    <div class="col-md-12">
+		                        <div class="md-form">
+		                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" placeholder="Your message" style="resize: none;"></textarea>
+		                            <label for="message"></label>
+		                        </div>
+		                    </div>
+		                </div>
+		            </form>
+		            <div class="text-center text-md-left">
+		                <a class="btn btn-primary" onclick="validateForm();">Send Message</a>
+		            </div>
+		            <div class="status"></div>
+		        </div>
+		    </div>
+		</section>
+	</div>
+</body>
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+	<script type="text/javascript">
+		document.getElementById('status').innerHTML = "Sending...";
+			formData = {
+			'name'     : $('input[name=name]').val(),
+			'email'    : $('input[name=email]').val(),
+			'subject'  : $('input[name=subject]').val(),
+			'message'  : $('textarea[name=message]').val()
+			};
+
+			//Ajax
+			$.ajax({
+			url : "mail.php",
+			type: "POST",
+			data : formData,
+			success: function(data, textStatus, jqXHR)
+			{
+
+			$('#status').text(data.message);
+			if (data.code) //If the e-mail was sent successfully, reset the form.
+			$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+			},
+			error: function (jqXHR, textStatus, errorThrown)
+			{
+			$('#status').text(jqXHR);
+			}
+			});
+	</script>
+</html>
